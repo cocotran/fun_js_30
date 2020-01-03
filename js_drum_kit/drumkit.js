b4ca64ac32c42c417edacc4289730552605e7125
QA <anhquangtran2703@gmail.com>
2020-01-03 21:27:31 +0700

    initial commit

--- js_drum_kit/drumkit.js
+++ js_drum_kit/drumkit.js
@@ -1,28 +1,30 @@
-function playing(event) {
-   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-   if (!audio) return;
-   audio.currentTime = 0;
-   audio.play();
-   key.classList.add('playing');
-};
+window.onload = function() {
+	function playing(event) {
+	   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+	   if (!audio) return;
+	   audio.currentTime = 0;
+	   audio.play();
+	   key.classList.add('playing');
+	};
 
-function removeTransition(event) {
-    if (event.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-};
+	function removeTransition(event) {
+	    if (event.propertyName !== 'transform') return;
+	    this.classList.remove('playing');
+	};
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+	const keys = document.querySelectorAll('.key');
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown', playing);
+	window.addEventListener('keydown', playing);
 
-function changeImg() {
-    const background = ["bg/bg-1.jpg", "bg/bg-2.jpg", "bg/bg-3.jpg", "bg/bg-4.jpg", "bg/bg-5.jpg", "bg-6.jpg", "bg/bg-7.jpg", "bg/bg-8.jpg", "bg/bg-9.jpg", "bg/bg-10.jpg"]
-    let num = Math.floor(Math.random() * background.length)
+	function changeImg() {
+	    const background = ["bg/bg-1.jpg", "bg/bg-2.jpg", "bg/bg-3.jpg", "bg/bg-4.jpg", "bg/bg-5.jpg", "bg-6.jpg", "bg/bg-7.jpg", "bg/bg-8.jpg", "bg/bg-9.jpg", "bg/bg-10.jpg"]
+	    let num = Math.floor(Math.random() * background.length)
 
-    let bg = document.body.style.backgroundImage
-    document.body.style.backgroundImage = 'url(' + background[num] + ')';
+	    let bg = document.body.style.backgroundImage
+	    document.body.style.backgroundImage = 'url(' + background[num] + ')';
+	}
+
+	window.onload = changeImg;
 }
-
-window.onload = changeImg;
